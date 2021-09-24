405eabecc8b42be0ca2412310b0b53f7affaf9d2
fkiefer <flokiefer@tuta.io>
2021-09-24 15:16:25 +0200

    Add comments to TableStreamValueParser

--- src/main/java/de/exxcellent/challenge/io/TableStreamValueParser.java
+++ src/main/java/de/exxcellent/challenge/io/TableStreamValueParser.java
@@ -8,33 +8,39 @@ import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
+
 public class TableStreamValueParser implements ValueParser<TableOfDoublesDataModel, Stream<String>>{
     private final String stringDelimiter = ",";
 
+    /**
+     * Method parseData()
+     * @param dataSource The data to be parsed into a table of doubles as a stream
+     * @return A Table of doubles containing the values of the data source
+     * @throws Exception if parsing goes wrong
+     */
     @Override
     public TableOfDoublesDataModel parseData(Stream<String> dataSource) throws Exception {
-        //check if input file exists and is readable
-        /**if(!dataSource.exists() || !dataSource.canRead()){
-            throw new Exception("Could not Read input File: File does not exist or is not readable");
-        }
-        BufferedReader reader = new BufferedReader(new FileReader(dataSource));
 
-        Stream<String> lineStream = reader.lines(); //read file line by line **/
-        List<LineOfDoublesDataModel> result = dataSource.skip(1)
-                .map(st -> {
+         List<LineOfDoublesDataModel> result = dataSource.skip(1) //skip first line which contains the labels
+                .map(st -> {//for every line in the stream:
                     try {
-                        ;
-                        return parseLine(st);
-                    } catch (Exception e) {
+                        return parseLine(st); //Parse the values into the desired format
+                    } catch (Exception e) { //print stacktrace if parsing goes wrong
                         e.printStackTrace();
                         return null;
                     }
-                }) // call function parseLine() for each line from the csv-file...
-                .collect(Collectors.toList());//...and collect the resulting line models in a list
+                })
+                .collect(Collectors.toList());// collect the resulting line models in a list
 
         return new TableOfDoublesDataModel(result.toArray(LineOfDoublesDataModel[]::new)); //collect all parsed lines in a table-model and return it
     }
 
+    /**
+     * Method parseLine takes a string (1 line) from the imput data and tries to parse a series of Double values which are then stored in the desired data structure
+     * @param line one line from the input data
+     * @return The parsed double values wrapped in an Object of LineOfDoublesDataModel
+     * @throws Exception if parsing goes wrong
+     */
     private LineOfDoublesDataModel parseLine(String line) throws Exception{
         //create tokenizer for delimiter specified above
         String columns[] = line.split(stringDelimiter);
