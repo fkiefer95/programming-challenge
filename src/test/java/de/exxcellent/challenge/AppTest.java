d8df7853a905efc16f11ddfb60d63eba6196c52b
Florian Kiefer <flokiefer@tuta.io>
2021-09-27 17:49:28 +0200

    Add test for WeatherValueCondition

--- src/test/java/de/exxcellent/challenge/AppTest.java
+++ src/test/java/de/exxcellent/challenge/AppTest.java
@@ -1,5 +1,8 @@
 package de.exxcellent.challenge;
 
+import de.exxcellent.challenge.applicationLogic.mathUtils.WeatherValueCondition;
+import de.exxcellent.challenge.dataModels.TableDataModel;
+import de.exxcellent.challenge.dataModels.WeatherLineDataModel;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
@@ -23,9 +26,17 @@ class AppTest {
         assertEquals("successful", successLabel, "My expectations were not met");
     }
 
-    @Test
+    /**@Test
     void runFootball() {
         App.main("--football", "football.csv");
-    }
+    }**/
 
+    @Test
+    void testWeatherValueCondition(){
+        double[] data = {1,2,3};
+        WeatherLineDataModel[] lineModel = {new WeatherLineDataModel(data)};
+        TableDataModel<WeatherLineDataModel> tableModel = new TableDataModel<>(lineModel);
+        WeatherValueCondition weatherValueConditionTestCandidate = new WeatherValueCondition();
+        assertEquals("1",weatherValueConditionTestCandidate.computeResult(tableModel),"Result incorrect");
+    }
 }
