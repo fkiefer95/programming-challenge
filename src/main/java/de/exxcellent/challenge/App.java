542e5fd5e06d440a2a1dfaff0a6a5453e2142c53
Florian Kiefer <flokiefer@tuta.io>
2021-09-28 15:37:02 +0200

    Implement commandline argument handling in main() in App Class

--- src/main/java/de/exxcellent/challenge/App.java
+++ src/main/java/de/exxcellent/challenge/App.java
@@ -17,28 +17,87 @@ public final class App {
      * @param args The CLI arguments passed
      */
     public static void main(String... args) {
-
+        //flags for execution of usecases
+        boolean runWeather = true;
+        boolean runFootball = true;
+        //filename to use (if provided)
+        String filename = null;
+        //check commandline arguments for which usecase to run
+        if(args.length > 0){
+            if(args[0].equals("--weather")){
+                runFootball = false;
+            } else if(args[0].equals("--football")){
+                runWeather = false;
+            } else{
+                System.out.println("Warning: Unrecognized arguments will be ignored\n");
+            }
+        }
+        //check commandline arguments for which file to use
+        if(args.length > 1){
+            filename = args[1];
+        }
+        //initialize display strings
         String dayWithSmallestTempSpread = "Error";
         String teamWithSmallestGoalSpread = "Error";
+        //run tasks
         try {
             //weather task
-            dayWithSmallestTempSpread = new WeatherTableInteractor()
-                    .processDataStructure(
-                            new CsvResourceTableDataProvider("de/exxcellent/challenge/weather.csv")
-                    ); // My day analysis function call …
-
+            if(runWeather) {
+                dayWithSmallestTempSpread = executeWeatherTask(filename);
+                System.out.printf("Day with smallest temperature spread : %s%n", dayWithSmallestTempSpread);
+            }
             //football task
-            teamWithSmallestGoalSpread = new FootballTableInteractor()
-                    .processDataStructure(
-                            new CsvResourceTableDataProvider("de/exxcellent/challenge/swag.csv")
-                    );// My goal analysis function call …
+            if(runFootball) {
+                teamWithSmallestGoalSpread = executeFootballTask(filename);
+                System.out.printf("Team with smallest goal spread       : %s%n", teamWithSmallestGoalSpread);
+            }
         } catch (Exception x){
             x.printStackTrace();
         }
 
-        System.out.printf("Day with smallest temperature spread : %s%n", dayWithSmallestTempSpread);
-        System.out.printf("Team with smallest goal spread       : %s%n", teamWithSmallestGoalSpread);
 
 
+
+
+    }
+
+    /**
+     * This method runs the weather usecase
+     * @param filename the name of the resource-file to use, may be null
+     * @return The number of the desired day
+     * @throws Exception if anything goes wrong
+     */
+    private static String executeWeatherTask(String filename) throws Exception{
+        if(filename == null) {
+            return new WeatherTableInteractor()
+                    .processDataStructure(
+                            new CsvResourceTableDataProvider("de/exxcellent/challenge/weather.csv")
+                    );// My day analysis function call …
+        } else{
+            return new WeatherTableInteractor()
+                    .processDataStructure(
+                            new CsvResourceTableDataProvider("de/exxcellent/challenge/" + filename.trim())
+                    );
+        }
+    }
+
+    /**
+     * This method runs the football usecase
+     * @param filename the name of the resource-file to use, may be null
+     * @return The name of the desired team
+     * @throws Exception if anything goes wrong
+     */
+    private static String executeFootballTask(String filename) throws Exception {
+        if (filename == null) {
+            return new FootballTableInteractor()
+                    .processDataStructure(
+                            new CsvResourceTableDataProvider("de/exxcellent/challenge/football.csv")
+                    );// My day analysis function call …
+        } else {
+            return new FootballTableInteractor()
+                    .processDataStructure(
+                            new CsvResourceTableDataProvider("de/exxcellent/challenge/" + filename.trim())
+                    );
+        }
     }
 }
